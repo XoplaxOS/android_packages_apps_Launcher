bbc11c3a1d1e7dd15187d47b7b63f5bff86d154c
Griffin Millender <griffinn.millender@gmail.com>
2015-01-23 00:39:08 -0600

    Fix drawer labels
    Change-Id: Ifbeaee592eaac84c3a29200f1b9c13adf9e86688
    Signed-off-by: AudioGod <adi.nth@outlook.com>

--- src/com/sokp/soniclauncher/Workspace.java
+++ src/com/sokp/soniclauncher/Workspace.java
@@ -597,6 +597,7 @@ public class Workspace extends SmoothPagedView
             throw new IllegalArgumentException("A Workspace can only have CellLayout children.");
         }
         CellLayout cl = ((CellLayout) child);
+        cl.setIsWorkspace(true);
         cl.setOnInterceptTouchListener(this);
         cl.setClickable(true);
         cl.setImportantForAccessibility(ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_NO);
@@ -679,6 +680,7 @@ public class Workspace extends SmoothPagedView
         CellLayout newScreen = (CellLayout)
                 mLauncher.getLayoutInflater().inflate(R.layout.workspace_screen, null);
 
+        newScreen.setIsWorkspace(true);
         newScreen.setOnLongClickListener(mLongClickListener);
         newScreen.setOnClickListener(mLauncher);
         newScreen.setSoundEffectsEnabled(false);
