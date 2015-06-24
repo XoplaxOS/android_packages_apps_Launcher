SlimRoms/packages_apps_SlimLauncher

Add backup & restore
Change-Id: Idc881113601945db77d3d6976a100624a106022c

src/com/slim/slimlauncher/settings/SettingsProvider.java
@@ -24,6 +24,10 @@ import java.util.Set;
 
 public class SettingsProvider implements SettingsKeys {
 
+    public static final String SETTINGS_KEY = "com.slim.slimlauncher_preferences";
+
+    public static final String KEY_SETTINGS_CHANGED = "settings_changed";
+
     public static SharedPreferences get(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context);
     }
@@ -104,6 +108,14 @@ public class SettingsProvider implements SettingsKeys {
 
     public static void putInt(Context context, String key, int value) {
         put(context).putInt(key, value);
+    }
+
+    public static void remove(Context context, String key) {
+        put(context).remove(key).commit();
+    }
+
+    public static void clear(Context context) {
+        put(context).clear().commit();
     }
 
     public static boolean shouldFinish(String key) {

XoplaxOS/android_packages_apps_Launcher

Signed-off-by: AudioGod <adi.nth@outlook.com>

src/com/sokp/soniclauncher/settings/SettingsProvider.java
@@ -22,6 +22,10 @@ import android.preference.PreferenceManager;
 
 public class SettingsProvider implements SettingsKeys {
 
+    public static final String SETTINGS_KEY = "com.sokp.soniclauncher_preferences";
+
+    public static final String KEY_SETTINGS_CHANGED = "settings_changed";
+
     public static SharedPreferences get(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context);
     }
@@ -94,6 +98,14 @@ public class SettingsProvider implements SettingsKeys {
 
     public static void putInt(Context context, String key, int value) {
         put(context).putInt(key, value);
+    }
+
+    public static void remove(Context context, String key) {
+        put(context).remove(key).commit();
+    }
+
+    public static void clear(Context context) {
+        put(context).clear().commit();
     }
 
     public static boolean shouldFinish(String key) {